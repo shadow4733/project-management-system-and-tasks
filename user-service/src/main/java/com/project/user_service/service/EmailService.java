c8f6197251a5cf7862ce56eefba88da8231715c8
shadow4733 <bogdan.rubanik28@gmail.com>
2024-07-21 03:57:10 +0700

    refactored interface

--- user-service/src/main/java/com/project/user_service/service/EmailService.java
+++ user-service/src/main/java/com/project/user_service/service/EmailService.java
@@ -1,9 +1,7 @@
 package com.project.user_service.service;
 
 import com.project.user_service.model.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface EmailService {
     void sendEmailVerificationToken(User user);
 }
--- user-service/src/main/java/com/project/user_service/service/UserService.java
+++ user-service/src/main/java/com/project/user_service/service/UserService.java
@@ -1,19 +1,14 @@
 package com.project.user_service.service;
 
-import com.project.user_service.dto.request.AddUserToTheProjectRequest;
 import com.project.user_service.dto.request.AuthenticationRequest;
 import com.project.user_service.dto.request.UpdatePasswordRequest;
 import com.project.user_service.dto.request.UserRequest;
-import com.project.user_service.dto.response.AddUserToTheProjectResponse;
 import com.project.user_service.dto.response.AuthenticationResponse;
 import com.project.user_service.dto.response.UpdatePasswordResponse;
 import com.project.user_service.dto.response.UserResponse;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
-@Service
 public interface UserService {
     UserResponse registerUser(UserRequest userRequest);
     void confirmEmail(String token);
