9b166e4eafba87c0241a75ebfd2eb6ff2d27db3f
shadow4733 <bogdan.rubanik28@gmail.com>
2024-07-27 01:12:29 +0700

    added new method in repository

--- project-management-service/src/main/java/com/project/project_management_service/repository/ProjectRepository.java
+++ project-management-service/src/main/java/com/project/project_management_service/repository/ProjectRepository.java
@@ -2,8 +2,12 @@ package com.project.project_management_service.repository;
 
 import com.project.project_management_service.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface ProjectRepository extends JpaRepository<Project, UUID>{
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+    boolean existsByProjectName(String projectName);
+    Project findByProjectName(String projectName);
 }
--- project-management-service/src/main/java/com/project/project_management_service/repository/VerifiedUserRepository.java
+++ project-management-service/src/main/java/com/project/project_management_service/repository/VerifiedUserRepository.java
@@ -4,8 +4,10 @@ import com.project.project_management_service.model.VerifiedUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VerifiedUserRepository extends JpaRepository<VerifiedUser, UUID> {
+    Optional<VerifiedUser> findByUsername(String username);
 }
